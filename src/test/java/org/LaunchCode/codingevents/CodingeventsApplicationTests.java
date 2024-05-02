3a99bd1584faed30e60f390e6a1a205b648bdb55
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-05-01 21:42:51 -0500

    Initial Commit

---
+++ src/test/java/org/LaunchCode/codingevents/CodingeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.codingevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
