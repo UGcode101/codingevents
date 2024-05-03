f3e0b1f51c374237101b9ee5ad3919d096afac1d
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-05-03 13:48:04 -0500

    complete

---
+++ src/main/java/org/LaunchCode/codingevents/ThymeleafConfig.java
@@ -0,0 +1,20 @@
+package org.LaunchCode.codingevents;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+}
