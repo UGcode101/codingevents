7d5c92a5ec63cad0ad2457fbe6699a02c8c1094e
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-05-06 21:27:32 -0500

    models and data

--- src/main/java/org/LaunchCode/codingevents/controllers/EventController.java
+++ src/main/java/org/LaunchCode/codingevents/controllers/EventController.java
@@ -1,11 +1,10 @@
 package org.LaunchCode.codingevents.controllers;
 
+import org.LaunchCode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +12,12 @@ import java.util.List;
 @RequestMapping("/events")
 public class EventController {
 
-    private List<String> events = new ArrayList<>(); // A simple list to store events
+    private static List<Event> events = new ArrayList<>();
 
     // Method to display all events
     @GetMapping
-    public String showEvents(Model model) {
+    public String showAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";  // Points to /templates/events/index.html
     }
@@ -28,10 +28,9 @@ public class EventController {
         return "events/create";  // Assumes you have a create.html in src/main/resources/templates/events/
     }
 
-    // Method to handle the creation of a new event
     @PostMapping("/create")
-    public String addEvent(@RequestParam String eventName, Model model) {
-        events.add(eventName);  // Add the new event to the list
-        return "redirect:/events";  // Redirect to the GET handler which shows the list
+    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.add(new Event(eventName, eventDescription));
+        return "redirect:/events";
     }
 }
